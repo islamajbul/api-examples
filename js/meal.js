75151dd509d70f093fa193317412151bb11da099
islamajabul <ajabulmd1@gmail.com>
2023-03-01 16:41:25 +0800

    class done

--- js/meal.js
+++ js/meal.js
@@ -24,6 +24,9 @@ const displayMeals = meals => {
         <div class="card-body">
           <h5 class="card-title">${meal.strMeal}</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <button onclick="loadMealDetail2(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+          Details
+          </button>
         </div>
         `
 
@@ -33,10 +36,42 @@ const displayMeals = meals => {
     });
 }
 
-const searchMeal = () =>{
+const searchMeal = () => {
     const searchText = document.getElementById('search-field').value;
     // Search meal
     console.log(searchText);
     loadMeals(searchText);
 }
-loadMeals('rice');
+
+const loadMealDetail = idMeal => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]))
+        .catch(error => {
+            console.log(error)
+        })
+
+}
+
+// async await
+const loadMealDetail2 = async (idMeal) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayMealDetails(data.meals[0]);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+const displayMealDetails = meal => {
+    document.getElementById('mealDetailsLabel').innerText = meal.strMeal;
+    const mealDetails = document.getElementById('mealDetailsBody');
+    mealDetails.innerHTML = `
+        <img class = "img-fluid" src = "${meal.strMealThumb}">
+    `
+}
+loadMeals('fish');
